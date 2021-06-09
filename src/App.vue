ae65c27b894d813f73ce8e3f1042c2130c21b037
booldook <booldook@gmail.com>
2021-06-09 15:03:08 +0900

    상품검색기 완료

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 		<Header @@selected="onSelected" />
 		<div class="container">
 			<TitleBar :title="title" :desc="desc" />
-			<SearchBar />
+			<SearchBar @@change="onChange" />
 			<ProductList :data="data" />
 		</div>
 	</div>
@@ -22,9 +22,13 @@ export default {
 	data() {
 		return {
 			title: '상품리스트',
-			desc: '',
-			product: null,
-			data: null,
+			product: [],
+			data: [],
+		}
+	},
+	computed: {
+		desc: function() {
+			return `상품이 ${this.data.length}개 준비되었습니다.`
 		}
 	},
 	async created() {
@@ -32,7 +36,6 @@ export default {
 			const { data } = await axios.get('/json/product.json')
 			this.product = data
 			this.data = data
-			this.changeDesc()
 		}
 		catch(err) {
 			console.log(err)
@@ -41,11 +44,10 @@ export default {
 	methods: {
 		onSelected(cate) {
 			this.data = (cate === '') ? this.product : this.product.filter( v => v.cate === cate )
-			this.changeDesc()
 		},
-		changeDesc() {
-			this.desc = `상품이 ${this.data.length}개 준비되었습니다.`
-		}
+		onChange(str) {
+			this.data = this.product.filter( v => v.title.includes(str) )
+		},
 	}
 }
 </script>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -14,6 +14,12 @@ export default {
 			query: '',
 		}
 	},
+	watch: {
+		query: function (v, l) {
+			// watch는 변수를 째려보다가 변수 값이 변화하면 기존값(l), 변화된 값(v)를 담아서 함수를 실행한다.
+			this.$emit('@change', v)
+		}
+	},
 	methods: {
 		onRemove() {
 			this.query = ''
